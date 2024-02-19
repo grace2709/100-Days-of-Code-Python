04a242256f3a7be3023cfaf5efec656a056b624f
Chiamaka  Anyanwu <93395990+grace2709@users.noreply.github.com>
2024-02-19 15:46:52 +0100

    Update main.py
    addes the format print in python

--- Day 1- Beginner-Working with Variables in Python to Manage Data/main.py
+++ Day 1- Beginner-Working with Variables in Python to Manage Data/main.py
@@ -2,3 +2,6 @@ print("Welcome to the Band Name Generator.")
 street = input("What's the name of the city you grew up in?\n")
 pet = input("What's your pet's name?\n")
 print("Your band name could be " + street + " " + pet)
+
+name = "Felix"
+print(f"your name is {name}")
